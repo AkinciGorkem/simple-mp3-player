1c3da32f44f726b78d9a6cf7a89da8dc56e9b647
AkinciGorkem <141235520+AkinciGorkem@users.noreply.github.com>
2023-08-03 21:56:25 +0300

    Pushed src files

---
+++ src/MainForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace MP3Player
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listBoxMusic = new System.Windows.Forms.ListBox();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.checkboxShuffle = new System.Windows.Forms.CheckBox();
+            this.checkboxRepeat = new System.Windows.Forms.CheckBox();
+            this.progressBarSong = new System.Windows.Forms.ProgressBar();
+            this.timerSong = new System.Windows.Forms.Timer(this.components);
+            this.labelSongDuration = new System.Windows.Forms.Label();
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxMusic
+            // 
+            this.listBoxMusic.FormattingEnabled = true;
+            this.listBoxMusic.Location = new System.Drawing.Point(12, 12);
+            this.listBoxMusic.Name = "listBoxMusic";
+            this.listBoxMusic.Size = new System.Drawing.Size(191, 95);
+            this.listBoxMusic.TabIndex = 0;
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(297, 12);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 2;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(390, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(297, 41);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrev.TabIndex = 5;
+            this.buttonPrev.Text = "Previous";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(209, 41);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 4;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // checkboxShuffle
+            // 
+            this.checkboxShuffle.AutoSize = true;
+            this.checkboxShuffle.Location = new System.Drawing.Point(225, 82);
+            this.checkboxShuffle.Name = "checkboxShuffle";
+            this.checkboxShuffle.Size = new System.Drawing.Size(59, 17);
+            this.checkboxShuffle.TabIndex = 6;
+            this.checkboxShuffle.Text = "Shuffle";
+            this.checkboxShuffle.UseVisualStyleBackColor = true;
+            this.checkboxShuffle.CheckedChanged += new System.EventHandler(this.checkBoxShuffle_CheckedChanged);
+            // 
+            // checkboxRepeat
+            // 
+            this.checkboxRepeat.AutoSize = true;
+            this.checkboxRepeat.Location = new System.Drawing.Point(297, 82);
+            this.checkboxRepeat.Name = "checkboxRepeat";
+            this.checkboxRepeat.Size = new System.Drawing.Size(61, 17);
+            this.checkboxRepeat.TabIndex = 7;
+            this.checkboxRepeat.Text = "Repeat";
+            this.checkboxRepeat.UseVisualStyleBackColor = true;
+            this.checkboxRepeat.CheckedChanged += new System.EventHandler(this.checkboxRepeat_CheckedChanged);
+            // 
+            // progressBarSong
+            // 
+            this.progressBarSong.Location = new System.Drawing.Point(91, 121);
+            this.progressBarSong.Name = "progressBarSong";
+            this.progressBarSong.Size = new System.Drawing.Size(312, 23);
+            this.progressBarSong.TabIndex = 8;
+            // 
+            // timerSong
+            // 
+            this.timerSong.Interval = 1000;
+            this.timerSong.Tick += new System.EventHandler(this.timerSong_Tick);
+            // 
+            // labelSongDuration
+            // 
+            this.labelSongDuration.AutoSize = true;
+            this.labelSongDuration.Location = new System.Drawing.Point(419, 127);
+            this.labelSongDuration.Name = "labelSongDuration";
+            this.labelSongDuration.Size = new System.Drawing.Size(34, 13);
+            this.labelSongDuration.TabIndex = 9;
+            this.labelSongDuration.Text = "00:00";
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.AutoSize = true;
+            this.labelCurrentTime.Location = new System.Drawing.Point(49, 127);
+            this.labelCurrentTime.Name = "labelCurrentTime";
+            this.labelCurrentTime.Size = new System.Drawing.Size(34, 13);
+            this.labelCurrentTime.TabIndex = 10;
+            this.labelCurrentTime.Text = "00:00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(240, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 158);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelCurrentTime);
+            this.Controls.Add(this.labelSongDuration);
+            this.Controls.Add(this.progressBarSong);
+            this.Controls.Add(this.checkboxRepeat);
+            this.Controls.Add(this.checkboxShuffle);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.listBoxMusic);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMusic;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.CheckBox checkboxShuffle;
+        private System.Windows.Forms.CheckBox checkboxRepeat;
+        private System.Windows.Forms.ProgressBar progressBarSong;
+        private System.Windows.Forms.Timer timerSong;
+        private System.Windows.Forms.Label labelSongDuration;
+        private System.Windows.Forms.Label labelCurrentTime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/MainForm.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MP3Player
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.Red, Color.Black, 90F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+
+        #region Init
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadMP3Files(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Musics"));
+            DisplayMP3FilesInListBox();
+        }
+        #endregion
+
+        #region Windows API
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder returnValue, int returnLength, IntPtr winHandle);
+
+        #endregion
+
+
+        #region Global Variables
+        private string aliasName = "MyMusic";
+        private int songLength = 0;
+        private List<string> originalSongList = new List<string>();
+        private Dictionary<string, int> songLengths = new Dictionary<string, int>();
+        private enum RepeatMode { NoRepeat, RepeatAll, RepeatOne };
+        private RepeatMode repeatMode = RepeatMode.NoRepeat;
+        #endregion
+        
+        #region Helper Functions
+        public void LoadMP3Files(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            var mp3Files = Directory.EnumerateFiles(directoryPath, "*.mp3", SearchOption.AllDirectories).ToList();
+            originalSongList = mp3Files;
+            foreach (string mp3File in mp3Files)
+                using (var file = TagLib.File.Create(mp3File))
+                    songLengths[mp3File] = (int)file.Properties.Duration.TotalMilliseconds;
+        }
+
+        public void DisplayMP3FilesInListBox()
+        {
+            listBoxMusic.Items.Clear();
+            foreach (string mp3File in originalSongList){
+                string fileName = Path.GetFileName(mp3File);
+                listBoxMusic.Items.Add(fileName);
+            }
+        }
+
+        public void PlayMP3(string fileName)
+        {
+            string absoluteFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            string command = $"open \"{absoluteFilePath}\" type mpegvideo alias {aliasName}";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            command = $"play {aliasName}";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            songLength = songLengths[absoluteFilePath];
+
+            progressBarSong.Maximum = songLength;
+            progressBarSong.Value = 0;
+
+            timerSong.Start();
+
+            TimeSpan duration = TimeSpan.FromMilliseconds(songLength);
+            labelSongDuration.Text = duration.ToString(@"mm\:ss");
+
+            this.Text = $"Currently Listening: {Path.GetFileNameWithoutExtension(fileName)}";
+        }
+
+        public void PauseMP3()
+        {
+            string command = $"pause {aliasName}";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            this.Text = "Paused the song...";
+        }
+
+        public void StopMP3()
+        {
+            string command = $"stop {aliasName}";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            command = $"close {aliasName}";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            timerSong.Stop();
+
+            progressBarSong.Value = 0;
+            labelCurrentTime.Text = "00:00";
+
+            this.Text = "Waiting for an action...";
+        }
+
+        public void NextSong()
+        {
+            if (listBoxMusic.Items.Count > 0)
+            {
+                StopMP3();
+                int nextIndex;
+                if (repeatMode == RepeatMode.RepeatAll)
+                    nextIndex = (listBoxMusic.SelectedIndex + 1) % listBoxMusic.Items.Count;
+                else if (repeatMode == RepeatMode.RepeatOne)
+                    nextIndex = listBoxMusic.SelectedIndex;
+                else
+                    nextIndex = (listBoxMusic.SelectedIndex + 1) % listBoxMusic.Items.Count;
+
+                listBoxMusic.SelectedIndex = nextIndex;
+                PlaySelectedSong();
+            }
+        }
+
+        public void PreviousSong()
+        {
+            if (listBoxMusic.Items.Count > 0)
+            {
+                StopMP3();
+                int previousIndex;
+                if (repeatMode == RepeatMode.RepeatAll)
+                    previousIndex = (listBoxMusic.SelectedIndex - 1 + listBoxMusic.Items.Count) % listBoxMusic.Items.Count;
+                else if (repeatMode == RepeatMode.RepeatOne)
+                    previousIndex = listBoxMusic.SelectedIndex;
+                else
+                    previousIndex = (listBoxMusic.SelectedIndex - 1 + listBoxMusic.Items.Count) % listBoxMusic.Items.Count;
+
+                listBoxMusic.SelectedIndex = previousIndex;
+                PlaySelectedSong();
+            }
+        }
+
+        public void PlaySelectedSong()
+        {
+            if (listBoxMusic.SelectedItem != null){
+                string fileName = listBoxMusic.SelectedItem.ToString();
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Musics", fileName);
+                PlayMP3(filePath);
+            }
+        }
+
+        public void ShuffleSongs()
+        {
+            Random rng = new Random();
+            int n = listBoxMusic.Items.Count;
+            while (n > 1){
+                n--;
+                int k = rng.Next(n + 1);
+                var value = listBoxMusic.Items[k];
+                listBoxMusic.Items[k] = listBoxMusic.Items[n];
+                listBoxMusic.Items[n] = value;
+            }
+        }
+        #endregion
+
+        #region Component Functions
+       
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            PauseMP3();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            StopMP3();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            NextSong();
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            PreviousSong();
+        }
+
+        private void checkBoxShuffle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkboxShuffle.Checked)
+                ShuffleSongs();
+            else{
+                listBoxMusic.Items.Clear();
+                foreach (string mp3File in originalSongList){
+                    string fileName = Path.GetFileName(mp3File);
+                    listBoxMusic.Items.Add(fileName);
+                }
+            }
+        }
+
+        private void checkboxRepeat_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkboxRepeat.Checked)
+                repeatMode = repeatMode == RepeatMode.NoRepeat ? RepeatMode.RepeatAll : RepeatMode.RepeatOne;
+            else
+                repeatMode = RepeatMode.NoRepeat;
+            checkboxRepeat.Text = repeatMode.ToString();
+        }
+
+        private void timerSong_Tick(object sender, EventArgs e)
+        {
+            StringBuilder position = new StringBuilder(32);
+            mciSendString($"status {aliasName} position", position, position.Capacity, IntPtr.Zero);
+            if (int.TryParse(position.ToString(), out int positionValue))
+                if (positionValue >= progressBarSong.Minimum && positionValue <= progressBarSong.Maximum)
+                    progressBarSong.Value = positionValue;
+            TimeSpan currentTime = TimeSpan.FromMilliseconds(progressBarSong.Value);
+            labelCurrentTime.Text = currentTime.ToString(@"mm\:ss");
+        }
+        #endregion
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            if (listBoxMusic.SelectedItem == null && listBoxMusic.Items.Count > 0)
+                listBoxMusic.SelectedIndex = 0;
+
+            if (listBoxMusic.SelectedItem != null)
+            {
+                string fileName = listBoxMusic.SelectedItem.ToString();
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Musics", fileName);
+                PlayMP3(filePath);
+            }
+        }
+    }
+}
